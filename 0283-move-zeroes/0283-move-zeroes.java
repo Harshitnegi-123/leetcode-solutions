9fcf7c4e12e7b278fddaf86fc1359e2ddfcf6a2d
Harshit Negi <harshunegi14@gmail.com>
2026-08-07 13:09:08 +0530

    Time: 2 ms (92.1%), Space: 48.1 MB (6.99%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int write = 0 ;
+        for(int read = 0 ; read < nums.length ; read++){
+            if(nums[read] != 0){
+                nums[write] = nums[read] ;
+                write++;
+            }
+        }
+        for( ; write < nums.length ; write++){
+            nums[write] = 0 ;
+        }
+    }
+}
